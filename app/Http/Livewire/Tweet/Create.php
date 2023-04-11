f765d2303953cb85c2b44ecd5b832f610d75fb06
rfl-designer <rfldesigner@outlook.com>
2023-04-11 14:34:29 -0300

    test: make sure that only authenticated users

--- app/Http/Livewire/Tweet/Create.php
+++ app/Http/Livewire/Tweet/Create.php
@@ -3,19 +3,30 @@
 namespace App\Http\Livewire\Tweet;
 
 use App\Models\Tweet;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Create extends Component
 {
+    use AuthorizesRequests;
     public ?string $body = null;
+
+    public function mount()
+    {
+        if(!auth()->check()) {
+            return redirect(route('login'));
+        }
+    }
 
     public function render()
     {
         return view('livewire.tweet.create');
     }
 
-    public function tweet():void 
+    public function tweet() 
     {
+        $this->authorize('create', Tweet::class);
+
         Tweet::query()->create([
             'body'       => $this->body,
             'created_by' => auth()->id(),
